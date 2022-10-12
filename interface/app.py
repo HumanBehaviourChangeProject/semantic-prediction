76cca760f85284610b139d4e0d2bb96be32b37ec
Janna Hastings <janna.hastings@babraham.ac.uk>
2022-10-12 16:02:08 +0200

    Add Python Shiny interface

---
+++ interface/app.py
@@ -0,0 +1,53 @@
+from shiny import App, render, ui
+
+import pickle
+import pandas as pd
+import numpy as np
+import torch
+
+import sys
+import pathlib
+import inspect
+import statistics
+import math
+
+import itertools
+
+src_file_path = inspect.getfile(lambda: None)
+
+PACKAGE_PARENT = pathlib.Path(src_file_path).parent
+PACKAGE_PARENT2 = PACKAGE_PARENT.parent
+SCRIPT_DIR = PACKAGE_PARENT2 / "rulenn"
+sys.path.append(str(PACKAGE_PARENT2))
+sys.path.append(str(SCRIPT_DIR))
+
+from rule_nn import RuleNet
+
+
+with open('data/hbcp_gen.pkl', "rb") as fin:
+    features, labels = pickle.load(fin)
+
+index = np.array(features.index)
+rename = [x[1] for x in features.columns]
+
+app_ui = ui.page_fluid(
+    ui.h2("Hello Shiny!"),
+    ui.input_slider("n", "N", 0, 100, 20),
+    ui.output_text_verbatim("txt"),
+    ui.output_text_verbatim("txtfeatures")
+)
+
+
+def server(input, output, session):
+    @output
+    @render.text
+    def txt():
+        return f"n*2 is {input.n() * 2}"
+
+    @output
+    @render.text
+    def txtfeatures():
+        return str(rename)
+
+
+app = App(app_ui, server)
